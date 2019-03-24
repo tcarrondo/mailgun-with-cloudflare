9ea89155b2279fa84e13425babb57b2353db13fb
Tiago Carrondo <tcarrondo@ubuntu.com>
2019-03-24 22:19:06 +0000

    initial code

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # mailgun-with-cloudflare
 A Terraform module to create a domain in Mailgun and add to Cloudflare all the DNS records needed to activate it.
+
+## Usage
+```hcl
+module "mailgun_domain_com" {
+  source = "github.com/tcarrondo/mailgun-with-cloudflare"
+
+  domain        = "domain.com"
+  smtp_password = "passwordwith32chars"
+}
+```
---
+++ main.tf
@@ -0,0 +1,43 @@
+# The DNS zone settings
+
+resource "cloudflare_zone_settings_override" "zone_settings" {
+  name    = "${var.domain}"
+
+  settings {
+    ssl = "full"
+    always_use_https = "on"
+    automatic_https_rewrites = "on"
+    cache_level = "aggressive"
+    development_mode = "off"
+  }
+}
+
+# Adding the domain to Mailgun
+
+resource "mailgunv3_domain" "domain" {
+    name          = "${var.domain}"
+    spam_action   = "disabled"
+    smtp_password = "${var.smtp_password}"
+}
+
+# Adding the DNS records to Cloudflare
+
+resource "cloudflare_record" "domain_mailgun_sending_records" {
+  count   = "3"
+  domain  = "${mailgunv3_domain.domain.name}"
+  name    = "${lookup(mailgunv3_domain.domain.sending_records[count.index], "name")}"
+  value   = "${lookup(mailgunv3_domain.domain.sending_records[count.index], "value")}"
+  type    = "${lookup(mailgunv3_domain.domain.sending_records[count.index], "record_type")}"
+  ttl     = 1
+  proxied = false
+}
+
+resource "cloudflare_record" "domain_mailgun_receiving_records" {
+  count   = "2"
+  domain  = "${mailgunv3_domain.domain.name}"
+  name    = "${mailgunv3_domain.domain.name}"
+  value   = "${lookup(mailgunv3_domain.domain.receiving_records[count.index], "value")}"
+  type    = "${lookup(mailgunv3_domain.domain.receiving_records[count.index], "record_type")}"
+  ttl     = 1
+  proxied = false
+}
